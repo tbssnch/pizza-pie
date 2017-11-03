28aa8c499b46a940ab7d5219cc00f1766e86eb97
Tyler Bussanich <tbussanich@gmail.com>
2017-11-03 10:42:34 -0700

    add form that allows user to select their desired topping. It will now output the users selected size and toppings

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,10 @@ $(document).ready(function() {
   $("form#pizza-order").submit(function(event) {
     event.preventDefault();
     var sizeInput = $("select#size").val();
+    var toppingInput = $("select#toppings").val();
 
     console.log();
     $("ul#order-list").text();
-    $("ul#order-list").append("<li>Size: " + sizeInput + "</li>");
+    $("ul#order-list").append("<li>Size: " + sizeInput + "</li>" + "<li>Toppings: " + toppingInput + "</li>");
   });
 });
