fab296ff6914d3dea13785f2791bc77a2e3f2113
Tyler Bussanich <tbussanich@gmail.com>
2017-11-03 15:30:24 -0700

    add statements that will output the price when the user selects any size with no topping and with cheese topping

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,34 @@ function Pizza (pizzaSize, pizzaTopping) {
 }
 
 Pizza.prototype.pizzaTotal = function() {
-  if (this.pizzaSize === "Small") {
+  if (this.pizzaSize === "Small" && this.pizzaTopping === "None") {
     this.total = 8;
     return this.total;
   }
-  else if (this.pizzaSize === "Medium") {
+  else if (this.pizzaSize === "Small" && this.pizzaTopping === "Cheese") {
+    this.total = 10;
+    return this.total;
+  }
+  else if (this.pizzaSize === "Medium" && this.pizzaTopping ==="None") {
     this.total = 12;
     return this.total;
   }
-  else if (this.pizzaSize === "Large") {
+  else if (this.pizzaSize === "Medium" && this.pizzaTopping === "Cheese") {
+    this.total = 14;
+    return this.total;
+  }
+  else if (this.pizzaSize === "Large" && this.pizzaTopping === "None") {
     this.total = 16;
     return this.total;
   }
+  else if (this.pizzaSize === "Large" && this.pizzaTopping === "Cheese") {
+    this.total = 18;
+    return this.total;
+  }
+  // else if (this.pizzaSize === "Small" && this.pizzaTopping === "Pepperoni") {
+  //   this.total = 10;
+  //   return this.total;
+  // }
 }
 
 $(document).ready(function() {
