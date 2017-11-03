4dc713dfe827ad43d2fe84ead1616016158fb786
Tyler Bussanich <tbussanich@gmail.com>
2017-11-03 14:45:50 -0700

    add constructer for pizza and prototype that will output the price if a user selects a small pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,27 @@
+function Pizza (pizzaSize, pizzaTopping) {
+  this.pizzaSize = pizzaSize;
+  this.pizzaTopping = pizzaTopping;
+  this.total = [];
+}
+
+Pizza.prototype.pizzaTotal = function() {
+  if (this.pizzaSize === "Small") {
+    this.total = 8;
+    return this.total;
+  }
+}
+
 $(document).ready(function() {
   $("form#pizza-order").submit(function(event) {
     event.preventDefault();
     var sizeInput = $("select#size").val();
     var toppingInput = $("select#toppings").val();
 
+
+    var newPizza = new Pizza(sizeInput, toppingInput);
+
     console.log();
     $("ul#order-list").text();
-    $("ul#order-list").append("<li>Size: " + sizeInput + "</li>" + "<li>Toppings: " + toppingInput + "</li>");
+    $("ul#order-list").append("<li>Size: " + sizeInput + "</li>" + "<li>Toppings: " + toppingInput + "</li>" + "<li>Price: " + newPizza.pizzaTotal() + "</li>");
   });
 });
